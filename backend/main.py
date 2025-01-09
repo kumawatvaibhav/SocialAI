e551dc990c2b124c5e0cd6c87845f9cdd0ab8de9
vaibhav <vaibhavkumawat21318@gmail.com>
2025-01-09 18:09:50 +0530

    remove env

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import requests
-from dotenv import load_dotenv
 import os
-
-load_dotenv()
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "https://social-ai-inky.vercel.app/"}}, supports_credentials=True)
